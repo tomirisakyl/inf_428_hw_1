4e85169af6585938b2ce8b5f2745d06a3885a414
tomirisakyl <210103212@stu.sdu.edu.kz>
2024-11-08 14:53:51 +0500

    Initial commit

--- test2.py
+++ test2.py
@@ -29,18 +29,63 @@ class TestCyclicTimeDifference(unittest.TestCase):
         self.assertAlmostEqual(cyclic_time_difference(0, 12), 12.0, places=2)
         self.assertAlmostEqual(cyclic_time_difference(12, 0), 12.0, places=2)
         
-    def test_cross_midnight(self):
+    def test_two_hour_cross_midnight(self):
         self.assertAlmostEqual(cyclic_time_difference(23, 1), 2.0, places=2)
         self.assertAlmostEqual(cyclic_time_difference(1, 23), 2.0, places=2)
     
-    def test_various_times(self):
-        self.assertAlmostEqual(cyclic_time_difference(3, 15), 12.0, places=2)
-        self.assertAlmostEqual(cyclic_time_difference(9, 21), 12.0, places=2)
-        self.assertAlmostEqual(cyclic_time_difference(6, 18), 12.0, places=2)
-
-    def test_large_cross_boundary_difference(self):
+    def test_four_hour_cross_midnight(self):
+        self.assertAlmostEqual(cyclic_time_difference(21, 1), 4.0, places=2)
         self.assertAlmostEqual(cyclic_time_difference(22, 2), 4.0, places=2)
-        self.assertAlmostEqual(cyclic_time_difference(1, 20), 5.0, places=2)
+
+    def test_six_hour_cross_midnight(self):
+        self.assertAlmostEqual(cyclic_time_difference(20, 2), 6.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(19, 1), 6.0, places=2)
+
+    def test_eight_hour_cross_midnight(self):
+        self.assertAlmostEqual(cyclic_time_difference(18, 2), 8.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(19, 3), 8.0, places=2)
+
+    def test_ten_hour_cross_midnight(self):
+        self.assertAlmostEqual(cyclic_time_difference(16, 2), 10.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(17, 3), 10.0, places=2)
+
+    def test_two_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(15, 17), 2.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(9, 11), 2.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(6, 8), 2.0, places=2)
+
+    def test_three_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(11, 14), 3.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(10, 13), 3.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(5, 8), 3.0, places=2)
+    
+    def test_four_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(9, 13), 4.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(7, 11), 4.0, places=2)
+    
+    def test_five_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(6, 11), 5.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(9, 14), 5.0, places=2)
+       
+    def test_six_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(6, 12), 6.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(9, 15), 6.0, places=2)
+    
+    def test_seven_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(6, 13), 7.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(9, 16), 7.0, places=2)
+    
+    def test_eight_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(6, 14), 8.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(9, 17), 8.0, places=2)
+
+    def test_nine_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(6, 15), 9.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(9, 18), 9.0, places=2)
+
+    def test_ten_hour_difference(self):
+        self.assertAlmostEqual(cyclic_time_difference(6, 16), 10.0, places=2)
+        self.assertAlmostEqual(cyclic_time_difference(9, 19), 10.0, places=2)
 
 if __name__ == '__main__':
     unittest.main()
