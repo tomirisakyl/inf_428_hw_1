17121e7ec41a57f44555861697360ad6f2b0a6e4
tomirisakyl <210103212@stu.sdu.edu.kz>
2024-11-08 14:13:06 +0500

    Initial commit

---
+++ task3.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+        set1 = set(nums1)
+        set2 = set(nums2)
+        
+        result = list(set1 & set2)
+        
+        return result
